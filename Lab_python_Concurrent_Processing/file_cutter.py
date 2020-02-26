ebf27fb8c106be4c4215f880260bef6721a7935f
Hello-Go-Hard <54393101+Hello-Go-Hard@users.noreply.github.com>
2020-02-26 13:02:47 +0300

    Update file_cutter

--- Lab_python_Concurrent_Processing/file_cutter.py
+++ Lab_python_Concurrent_Processing/file_cutter.py
@@ -21,21 +21,11 @@ def separate_file(data, count_of_files):
 
 
 def write_to_csv(data, file_name):
-    print('i am '+file_name)
     file_output = open(file_name, 'w', newline='')
     writer = csv.DictWriter(file_output, fieldnames=tuple(data[0].keys()), delimiter='|')
     writer.writeheader()
     for row in data:
         writer.writerow(row)
-    print('write '+file_name)
     file_output.close()
     return True
 
-
-if __name__ == '__main__':
-    start = time.time()
-    data_dict = scan_file('cstmc-CSV-en.csv')
-    files = separate_file(data_dict, 4)
-    write_to_csv(files[0], 'first_csv_output.csv')
-    print('The end')
-    print(time.time() - start)
